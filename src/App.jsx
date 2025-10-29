d55110c6cbb1b59d1cba63422f5fcf7d1122fa7f
sasi-06 <sasisivaprakashm@gmail.com>
2025-10-29 12:19:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,30 @@
-import { useState,useEffect } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [user, setUser] = useState([])
-  const [form, setForm] = useState({name:"",email:"",password:""})
-  const [editId,setEditId]=useState(null)
+  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    const saved=JSON.parse(localStorage.getItem(user));
-    setUser(user);
-  },[]);
-  useEffect(() => {
-    const newuser=(localStorage.setItem("user"))
-    setUser(...user,newuser)
-  },[user]);
-  const handlesubmit = (e) =>{
-    e.preventdefault();
-    if(!name || !email || !password) return
-    if(editId){
-      setUser(user.map(u) = ( u.id === id ?{...form,id:name}:u))
-      setEditId(null);
-    }
-    else{
-      const newuser=setUser({id:name,newuser})
-      setForm(...user,newuser)
-    }
-  };
-  const handlesubmitpass = (pass) =>{
-    // setUser(user.map(u) = ({u.pass ===password})?password:u)
-  }
-  retrun(
-    <form onClick={handlesubmit()}>
-      <input type='text'  value={form.name}placeholder='Enter the name' onChange={(e) => setForm(name,e.target.value)}></input>
-      <input type='email' value={form.email}placeholder='Enter the email' onChange={(e)=>setForm(email,e.target.value)}></input>
-      <input type='password' value={form.email}placeholder='Enter the password' onChange={(e)=>setForm(password,e.target.value)}></input>
+  return (
+    <>
       <div>
-        {user.length === 0 ?(<p>No user added</p>):
-         <ul>
-          <li key={u.id}>
-            {user.map(u) = (
-              <button onClick={handlesubmitpass}></button>
-            )}
-          </li>
-          </ul>}
+        <input type='text'  value={form.name}placeholder='Enter the name' onChange={(e) => setForm(name,e.target.value)}></input>
       </div>
-    </form>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
-  
-export default App;
+
+export default App
